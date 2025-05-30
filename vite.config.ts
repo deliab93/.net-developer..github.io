baeb590748d20e0cf41bf1bb2415d4346e5eea19
BDM <deliaab93@gmail.com>
2025-05-30 10:14:27 +0100

    added changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/.net-developer.github.io/',  // 👈 Important for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
+
